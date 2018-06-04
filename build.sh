736c604a67f648118b8ae653c64f3fdf87590ba3
Gwilym Evans <gwilym.evans@bigcommerce.com>
2018-06-04 22:55:14 +0000

    begin adding cleanup to reduce image size

--- build.sh
+++ build.sh
@@ -27,41 +27,49 @@ cd kafka
 sed -ie 's#"-Xlint:unsound-match"#"-Xlint:unsound-match","-Xmax-classfile-name","127"#' build.gradle
 gradle
 ./gradlew installAll
+rm -rf .git
 
 cd /opt
 git clone https://github.com/confluentinc/common confluentinc-common
 cd confluentinc-common
 mvn install -Dmaven.test.skip=true
+rm -rf .git
 
 cd /opt
 git clone https://github.com/confluentinc/rest-utils confluentinc-rest-utils
 cd confluentinc-rest-utils
 mvn install -Dmaven.test.skip=true
+rm -rf .git
 
 cd /opt
 git clone https://github.com/confluentinc/schema-registry confluentinc-schema-registry
 cd confluentinc-schema-registry
 mvn install -Dmaven.test.skip=true
+rm -rf .git
 
 cd /opt
 git clone https://github.com/confluentinc/kafka-connect-storage-common kafka-connect-storage-common
 cd kafka-connect-storage-common
 mvn install package -Dmaven.test.skip=true
+rm -rf .git
 
 cd /opt
 git clone https://github.com/confluentinc/kafka-connect-storage-cloud kafka-connect-storage-cloud
 cd kafka-connect-storage-cloud
 mvn package -Dmaven.test.skip=true
 ln -s /opt/kafka-connect-storage-common/package-kafka-connect-storage-common/target/kafka-connect-storage-common-package-5.0.0-SNAPSHOT-package/share/java/kafka-connect-storage-common kafka-connect-s3/target/kafka-connect-s3-5.0.0-SNAPSHOT-package/share/java/kafka-connect-s3/kafka-connect-storage-common
+rm -rf .git
 
 # configure
 sed -ie 's/^#networkaddress.cache.ttl=-1$/networkaddress.cache.ttl=30/' /root/.sdkman/candidates/java/current/jre/lib/security/java.security
 
-# TODO: might be able to remove all the dependency repos (except kafka itself
-# since we need the bin/ scripts, but that could be remedied in future)
-
 sdk flush temp
 sdk flush archives
+sdk flush broadcast
+
+cd /
+rm -rf /root/.gradle /root/.m2
+rm -rf /opt/confluentinc-common /opt/confluentinc-rest-utils /opt/confluentinc-schema-registry /opt/kafka-connect-storage-common
 
 groupadd -g 30010 -r connect
 useradd -u 30010 --no-log-init -g connect connect
